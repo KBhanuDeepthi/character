797e52842f5109de013d31a337e0e937fc9bd691
KBhanuDeepthi <bhanudeepthivinni915@gmail.com>
2022-08-29 12:54:48 +0530

    Added: digit-character logic

---
+++ character.sh
@@ -0,0 +1,31 @@
+read -p "enter the num:" num
+if((num=="1"))
+then
+    echo "one"
+elif((num=="2"))
+then
+    echo "two"
+elif((num=="3"))
+then
+    echo "three"
+elif((num=="4"))
+then
+    echo "Four"
+elif((num=="5"))
+then
+    echo "Five"
+elif((num=="6"))
+then
+    echo "Six"
+elif((num=="7"))
+then
+    echo "Seven"
+elif((num=="8"))
+then
+    echo "Eight"
+elif((num=="9"))
+then
+    echo "Nine"
+else
+    echo "It is not a single digit"
+fi
